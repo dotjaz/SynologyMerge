64989a2760b42af624b685346df208b42b9893ad
JZ <5112407+dotjaz@users.noreply.github.com>
2023-08-04 01:31:11 +1000

    Updated DSM7.x compatibility, improving usability

--- README.md
+++ README.md
@@ -8,16 +8,16 @@ This script is in Beta and hanven't been tested on divices. Use at your own risk
 1. Log in to Synology NAS using SSH as admin.
 2. Run `sudo -i` to change user to root
 3. Clone this repository or `wget` the raw file to a safe location, usually root user home
-4. Mark the `.sh` file or the `.x` binary as executable by `chmod +x migrate.sh` or `chmod +x migrate.sh.x`
-5. Determine the target and destination volume by navigating to `/`
-6. `cd` into the Target volume `@appstore`, aka. the volume you want to delete by running `cd /volume<target volume #>/\@appstore`
-7. Check the packages and run the script by `~/migrate.sh -f <Target Volume> -t <Destination Volume>`, `-a` optional for automatic migrate without prompt
+4. Mark the `.sh` file as executable by `chmod +x migrate.sh`
+5. Determine the Source and Target volume by navigating to `/`
+6. `cd` into the Source volume `@appstore`, aka. the volume you want to delete by running `cd /volume<target volume #>/\@appstore`
+7. Check the packages and run the script by `~/migrate.sh -s <Source Volume> -t <Target Volume>`, `-a` optional for automatic migrate without prompt
 
 ### Using the Script
 
 |Option|Description|
 |------|---|
-| `-f` |Target(from) volume, should be in the form of `volumeX` e.g. `volume1`|
-| `-t` |Destination(to) volume, should be in the form of `volumeX` e.g. `volume2`|
+| `-s` |Source(from) volume, should be in the form of `volumeX` e.g. `volume1`|
+| `-t` |Target(to) volume, should be in the form of `volumeX` e.g. `volume2`|
 | `-h` |Show Help|
-| `-a` |If specified, will migrate all packages in current directory without prompts (Warning: Be careful! Always check packages)|
+| `-a` |If specified, will migrate all packages in current volume without prompts (Warning: Be careful! Always check packages)|
--- migrate.sh
+++ migrate.sh
@@ -1,75 +1,119 @@
 #!/bin/bash
 set -e
 Dir(){
- if [ ! -d /$TO/\@appstore ]; then
-   mkdir -p /$TO/\@appstore
+ if [ ! -d /$TARGET/\@appstore ]; then
+   mkdir -p /$TARGET/\@appstore
  fi
 }
 Help(){
-	printf '\n%s\n\t%s' '-f' 'Target(from) volume, should be in the form of volumeX'
-	printf '\n%s\n\t%s' '-t' 'Destination(to) volume, should be in the form of volumeX'
-	printf '\n%s\n\t%s' '-a' 'If specified, will migrate all packages in current directory(Warning: careful!)'
-
+	printf '\n%s\n\t%s' '-s' 'Source (source) volume, should be in the form of volumeX'
+	printf '\n%s\n\t%s' '-t' 'Destination (target) volume, should be in the form of volumeX'
+	printf '\n%s\n\t%s' '-a' 'If specified, will migrate all packages in current directory (Warning: careful!)\n'
 	exit 1
 }
 All(){
 Dir
-for f in /$FROM/\@appstore/*
+for f in /$SOURCE/\@appstore/*
 do 
 	pkg="${f##*/}"
 	echo $pkg
-	/var/packages/$pkg/scripts/start-stop-status stop
-	mv /$FROM/\@appstore/$pkg /$TO/\@appstore
-	rm /var/packages/$pkg/target
-	ln -s /$TO/\@appstore/$pkg /var/packages/$pkg/target
-	ls -l /var/packages/$pkg
-	/var/packages/$pkg/scripts/start-stop-status start
+	[ -e /var/packages/$pkg/scripts/start-stop-status ] && /var/packages/$pkg/scripts/start-stop-status stop &
+	sleep 5
+	[ -e /var/packages/$pkg/target ] && rm -rf /var/packages/$pkg/target
+	[ -e /var/packages/$pkg/etc ] && rm -rf /var/packages/$pkg/etc
+	[ -e /var/packages/$pkg/home ] && rm -rf /var/packages/$pkg/home
+	[ -e /var/packages/$pkg/tmp ] && rm -rf /var/packages/$pkg/tmp
+	[ -e /var/packages/$pkg/var ] && rm -rf /var/packages/$pkg/var
+	[ -e /$SOURCE/\@appstore/$pkg ] && mv /$SOURCE/\@appstore/$pkg /$TARGET/\@appstore && ln -s /$TARGET/\@appstore/$pkg /var/packages/$pkg/target
+	[ -e /$SOURCE/\@appconf/$pkg ] && mv /$SOURCE/\@appconf/$pkg /$TARGET/\@appconf && ln -s /$TARGET/\@appconf/$pkg /var/packages/$pkg/etc
+	[ -e /$SOURCE/\@apphome/$pkg ] && mv /$SOURCE/\@apphome/$pkg /$TARGET/\@apphome && ln -s /$TARGET/\@apphome/$pkg /var/packages/$pkg/home
+	[ -e /$SOURCE/\@apptemp/$pkg ] && mv /$SOURCE/\@apptemp/$pkg /$TARGET/\@apptemp && ln -s /$TARGET/\@apptemp/$pkg /var/packages/$pkg/tmp
+	[ -e /$SOURCE/\@appdata/$pkg ] && mv /$SOURCE/\@appdata/$pkg /$TARGET/\@appdata && ln -s /$TARGET/\@appdata/$pkg /var/packages/$pkg/var
+done
+	mv /$SOURCE/\@appstore/* /$TARGET/\@appstore
+	mv /$SOURCE/\@appconf/* /$TARGET/\@appconf
+	mv /$SOURCE/\@apphome/* /$TARGET/\@apphome
+	mv /$SOURCE/\@apptemp/* /$TARGET/\@apptemp
+	mv /$SOURCE/\@appdata/* /$TARGET/\@appdata
+
+for f in /$TARGET/\@appstore/*
+do 
+	pkg="${f##*/}"
+	echo $pkg
+	[ -e /$TARGET/\@appstore/$pkg ] && ln -sf /$TARGET/\@appstore/$pkg /var/packages/$pkg/target
+	[ -e /$TARGET/\@appconf/$pkg ] && ln -sf /$TARGET/\@appconf/$pkg /var/packages/$pkg/etc
+	[ -e /$TARGET/\@apphome/$pkg ] && ln -sf /$TARGET/\@apphome/$pkg /var/packages/$pkg/home
+	[ -e /$TARGET/\@apptemp/$pkg ] && ln -sf /$TARGET/\@apptemp/$pkg /var/packages/$pkg/tmp
+	[ -e /$TARGET/\@appdata/$pkg ] && ln -sf /$TARGET/\@appdata/$pkg /var/packages/$pkg/var
 done
 exit 1
 }
+
 Run(){
 Dir
-for f in /$FROM/\@appstore/*
+for f in /$SOURCE/\@appstore/*
 do 
 	pkg="${f##*/}"
 	echo $pkg
 	read -p "proceed?(y/n) " pcd
 	if [ "$pcd" == "y" ]; then
-		/var/packages/$pkg/scripts/start-stop-status stop
-		mv /$FROM/\@appstore/$pkg /$TO/\@appstore
-		rm /var/packages/$pkg/target
-		ln -s /$TO/\@appstore/$pkg /var/packages/$pkg/target
+		[ -e /var/packages/$pkg/scripts/start-stop-status ] && /var/packages/$pkg/scripts/start-stop-status stop &
+		sleep 5
+		[ -e /var/packages/$pkg/target ] && rm -rf /var/packages/$pkg/target
+		[ -e /var/packages/$pkg/etc ] && rm -rf /var/packages/$pkg/etc
+		[ -e /var/packages/$pkg/home ] && rm -rf /var/packages/$pkg/home
+		[ -e /var/packages/$pkg/tmp ] && rm -rf /var/packages/$pkg/tmp
+		[ -e /var/packages/$pkg/var ] && rm -rf /var/packages/$pkg/var
+		[ -e /$SOURCE/\@appstore/$pkg ] && mv /$SOURCE/\@appstore/$pkg /$TARGET/\@appstore && ln -s /$TARGET/\@appstore/$pkg /var/packages/$pkg/target
+		[ -e /$SOURCE/\@appconf/$pkg ] && mv /$SOURCE/\@appconf/$pkg /$TARGET/\@appconf && ln -s /$TARGET/\@appconf/$pkg /var/packages/$pkg/etc
+		[ -e /$SOURCE/\@apphome/$pkg ] && mv /$SOURCE/\@apphome/$pkg /$TARGET/\@apphome && ln -s /$TARGET/\@apphome/$pkg /var/packages/$pkg/home
+		[ -e /$SOURCE/\@apptemp/$pkg ] && mv /$SOURCE/\@apptemp/$pkg /$TARGET/\@apptemp && ln -s /$TARGET/\@apptemp/$pkg /var/packages/$pkg/tmp
+		[ -e /$SOURCE/\@appdata/$pkg ] && mv /$SOURCE/\@appdata/$pkg /$TARGET/\@appdata && ln -s /$TARGET/\@appdata/$pkg /var/packages/$pkg/var
 		ls -l /var/packages/$pkg
-		/var/packages/$pkg/scripts/start-stop-status start
-	else
-		break
+		[ -e /var/packages/$pkg/scripts/start-stop-status ] && /var/packages/$pkg/scripts/start-stop-status start &
 	fi
 done
 }
 
-FROM='NONE'
-TO='NONE'
+Start(){
+Dir
+for f in /$TARGET/\@appstore/*
+do 
+	pkg="${f##*/}"
+	echo $pkg
+	read -p "proceed?(y/n) " pcd
+	if [ "$pcd" == "y" ]; then
+		[ -e /var/packages/$pkg/scripts/start-stop-status ] && /var/packages/$pkg/scripts/start-stop-status start &
+		sleep 5
+	fi
+done
+}
 
-opts=":f:t:ah"
+SOURCE='NONE'
+TARGET='NONE'
+
+opts=":s:t:ah"
 while getopts $opts option
 do
 	case ${option} in
-		f) FROM=${OPTARG};;
-		t) TO=${OPTARG};;
+		s) SOURCE=${OPTARG};;
+		t) TARGET=${OPTARG};;
 		h)
 			Help
 			;;
-		a)
-			All
-			;;
+		a) ALLPKG=1;;
 		\? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
 		:  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
 		*  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
 	esac
 done
 
-if [ $FROM != 'NONE' ] && [ $TO != 'NONE' ]; then
+if [ $SOURCE != 'NONE' ] && [ $TARGET != 'NONE' ] && [ $ALLPKG = 1 ]; then
+	All
+	Start
+else if [ $SOURCE != 'NONE' ] && [ $TARGET != 'NONE' ]; then
 	Run
+	Start
 else
 	Help
-fi
+fi; fi
